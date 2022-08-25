5e8cb6d5df5d2e868b9a4477ed90aeb6baf66a93
Gokhan <gknsvsdata@gmail.com>
2022-08-25 18:28:54 +0300

    last

--- student/views.py
+++ student/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import StudentForm
 
-# Create your views here.
+def index(request):
+    return render(request, "student/index.html")
+
+def student_page(request):
+    return render(request, "student/student.html")
+
+def student_page(request):
+    print(request.POST)
+    print(request.FILES)
+    form = StudentForm()
+    contex = {
+        "form": form
+    }
+
+    return render(request, "student/student.html", contex)
