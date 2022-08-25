7d8c15b480ec0c6e28d067174de093a752e2b288
Gokhan <gknsvsdata@gmail.com>
2022-08-25 19:29:18 +0300

    last commit

--- student/forms.py
+++ student/forms.py
@@ -1,8 +1,19 @@
 from django import forms
+from .models import Student
 
 
-class StudentForm(forms.Form):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    number = forms.IntegerField(required=False)
-    profile_image = forms.ImageField(required=False)
+# class StudentForm(forms.Form):
+#     first_name = forms.CharField(max_length=30)
+#     last_name = forms.CharField(max_length=30)
+#     number = forms.IntegerField(required=False)
+#     profile_image = forms.ImageField(required=False)
+
+
+
+
+class StudentForm(forms.ModelForm):
+
+    class Meta:
+        model = Student
+        fields = ["first_name", "last_name", "number", "profile_pic"]  # __all__ yaparsak hepsini secer
+        labels = {"first_name": "Name"}
--- student/views.py
+++ student/views.py
@@ -1,18 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Student
 from .forms import StudentForm
 
 def index(request):
     return render(request, "student/index.html")
 
-def student_page(request):
-    return render(request, "student/student.html")
+# def student_page(request):
+#     return render(request, "student/student.html")
+
+# def student_page(request):
+#     print(request.POST)
+#     print(request.FILES)
+#     form = StudentForm()
+#     contex = {
+#         "form": form
+#     }
+
+#     return render(request, "student/student.html", contex)
+
+# def student_page(request):
+#     form = StudentForm()
+#     if request.method == "POST":
+#         form = StudentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             student_data = {
+#                 "first_name": form.cleaned_data.get("first_name"),
+#                 "last_name": form.cleaned_data.get("last_name"),
+#                 "number": form.cleaned_data.get("number"),
+#                 "profile_pic": form.cleaned_data.get("profile_image")
+#             }
+
+
+#             # student.object.create(**student_data)
+
+#             student = Student(**student_data)
+#             student.save()
+#             return redirect("student")
+
+#         context = {
+#             "form" : form
+#         }
+
+#         return render(request, "student/student.html", context)
+
 
 def student_page(request):
-    print(request.POST)
-    print(request.FILES)
     form = StudentForm()
-    contex = {
-        "form": form
-    }
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("student")
+    context = {
+            "form" : form
+        }
 
-    return render(request, "student/student.html", contex)
+    return render(request, "student/student.html", context)
